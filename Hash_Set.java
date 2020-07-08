52e49c11f3feb0ab8604fa4a68ead098de82876c
siddhi9108 <66294249+siddhi9108@users.noreply.github.com>
2020-07-08 15:54:04 +0530

    Add files via upload

---
+++ Hash_Set.java
@@ -0,0 +1,18 @@
+package javaapplication2;
+
+import java.util.HashSet;
+
+public class Hash_Set {
+
+    public static void main(String[] args) {
+        HashSet<Integer> ob = new HashSet<>();
+        ob.add(10);
+        ob.add(50);
+        ob.add(44);
+        ob.add(23);
+        ob.add(10);
+        System.out.println(ob);
+
+    }
+
+}
